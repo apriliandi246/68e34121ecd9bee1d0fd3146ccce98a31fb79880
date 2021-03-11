67a75d6cfd7a91246424bd1d6f4196ab6a4ee86b
Farhan Apriliandi <apriliandi642@gmail.com>
2021-03-12 02:15:01 +0700

    add utils for functionality

---
+++ src/utils/utils.js
@@ -0,0 +1,97 @@
+export const foods = [
+   {
+      name: "NUSA by Betawi Corner",
+      price: "Rp 35,000",
+      urlImg: "https://kln.imgix.net/production/nusa-by-kafe-betawi.jpeg",
+   },
+   {
+      name: "Ayam Goreng Serai",
+      price: "Rp 70,000",
+      urlImg: "https://kln.imgix.net/production/5UDJJOS97W-Savoury-2.jpg?w=700",
+   },
+   {
+      name: "NUSA Cumi Pedas",
+      price: "Rp 50,500",
+      urlImg: "https://kln.imgix.net/production/nusa-cumi-pedas.jpeg?w=700",
+   },
+   {
+      name: "Hainamese Chicken Salted Egg Rice",
+      price: "Rp 33,000",
+      urlImg: "https://kln.imgix.net/production/CXNA1A4G4E-P2040059.JPG?w=700",
+   },
+   {
+      name: "Ayam Balado",
+      price: "Rp 22,914",
+      urlImg:
+         "https://kln.imgix.net/production/2ecddc2a98083bfab1b503334afbeeeb_Ayam_Balado-Tempe_Goreng.jpg?w=700",
+   },
+   {
+      name: "Nasi Dori Asam Manis",
+      price: "Rp 25,000",
+      urlImg:
+         "https://kln.imgix.net/production/M5D01I5D5P-nasi-dori-asam-manis.jpg?w=700",
+   },
+   {
+      name: "Nasi Ayam Bakar dan Bakwan Jagung",
+      price: "Rp 34,000",
+      urlImg:
+         "https://kln.imgix.net/production/photo6314369290203146855.jpg?w=700",
+   },
+];
+
+export const dates = [
+   {
+      day: "SEN",
+      date: 10,
+   },
+   {
+      day: "SEL",
+      date: 11,
+   },
+   {
+      day: "RAB",
+      date: 12,
+   },
+   {
+      day: "KAM",
+      date: 13,
+   },
+   {
+      day: "JUM",
+      date: 14,
+   },
+   {
+      day: "SAB",
+      date: 15,
+   },
+   {
+      day: "MIN",
+      date: 16,
+   },
+];
+
+export const mediaQueries = `
+   @media only screen and (min-width: 635px) {
+      width: 80%;
+   }
+
+   @media only screen and (min-width: 714px) {
+      width: 70%;
+   }
+
+   @media only screen and (min-width: 810px) {
+      width: 60%;
+   }
+
+   @media only screen and (min-width: 1110px) {
+      width: 50%;
+   }
+
+   @media only screen and (min-width: 1250px) {
+      width: 40%;
+   }
+
+   @media only screen and (min-width: 1600px) {
+      width: 30%;
+   }
+`;
